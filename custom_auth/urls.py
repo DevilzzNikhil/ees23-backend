8192c2138b0ef57177b8b5f5f641dc1d8305d458
Nikhil <nikhil10july123@gmail.com>
2022-12-13 19:35:04 +0530

    backend updated

--- custom_auth/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .api import UserInitApi
-
-
-urlpatterns = [
-    path('google-login/', UserInitApi.as_view(), name='google-login' )
-]
--- udyam_API/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UdyamApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'udyam_API'
