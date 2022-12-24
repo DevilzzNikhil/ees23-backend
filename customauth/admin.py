09023b595d8792c4d8370b2463ee1e53a4d2e797
Archit Pattanaik <98776672+archit-2003@users.noreply.github.com>
2022-12-24 23:22:27 +0530

    Delete customauth directory

--- customauth/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from customauth.models import UserAcount
-admin.site.register(UserAcount)
--- customauth/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import UserInitApi
-
-urlpatterns = [
-    path('google-login/', UserInitApi.as_view(), name='google-login'),
-]
