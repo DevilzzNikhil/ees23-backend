057ad00eaa6e8ed5788ead606bdbca0d00a8fc4c
Samar Singh Randhawa <98534735+Samar1110@users.noreply.github.com>
2022-12-29 19:49:54 +0530

    Update urls.py

--- customauth/urls.py
+++ customauth/urls.py
@@ -4,5 +4,5 @@ from .admin import BroadCast_Email_Admin
 urlpatterns = [
     path('google-login/', UserInitApi.as_view(), name='google-login'),
     path("broadcast/<subject>/", broadcast_mail, name="broadcast_mail"),
-    path("goMail", index),
+    path("broadcaster", index),
 ]
